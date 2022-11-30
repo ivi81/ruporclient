94b5d630189fab90e16dabe516be884dc9acc8ae
Илья Ипполитов <i.ippolitov@cloud.gcm>
2022-11-30 11:29:50 +0200

    30.11.2022 FIX bug in rupor/config/config.go

--- rupor/config/config.go
+++ rupor/config/config.go
@@ -5,7 +5,7 @@ import "gitlab.cloud.gcm/i.ippolitov/go-microconfig/microconfig"
 
 //RuporClientCfg - структура данных описывающая информацию о конфигурации модуля логирования микросервисов
 type RuporClientCfg struct {
-	microconfig.ClientHttpCfg
+	microconfig.ClientHttpCfg `yaml:",inline"`
 }
 
 //SetValuesFromEnv загружает значение перменных среды которые имеют префикс заданный в envPrefix
