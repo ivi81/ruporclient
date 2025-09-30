7d4e37a4ed02a10c5c0e2d15e9bc1585ce31b465
Илья Ипполитов <i.ippolitov@cloud.gcm>
2025-09-30 12:23:53 +0300

    FIX ClientRuporCfg in config.go

--- config/config.go
+++ config/config.go
@@ -1,10 +1,15 @@
 // config.go описание конфигурации клиента RUPOR
 package config
 
+import "time"
+
 // ClientRuporCfg - структура данных описывающая информацию о конфигурации модуля логирования микросервисов
 type ClientRuporCfg struct {
-	Host   string `yaml:"url" env:"HOST"`
-	Port   int    `yaml:"port" env:"PORT"`
-	ApiKey string `yaml:"apiKey" env:"API_KEY"`
-	UserAgent string `yaml:"userAgent" env:"USER_AGENT"`
+	Host          string        `yaml:"url" env:"HOST"`
+	Port          int           `yaml:"port" env:"PORT"`
+	ApiKey        string        `yaml:"apiKey" env:"API_KEY"`
+	UserAgent     string        `yaml:"userAgent" env:"USER_AGENT"`
+	Timeout       time.Duration `yaml:"timeout"`
+	SleepTime     time.Duration `yaml:"sleepTime"`
+	ResponseLimit int           `yaml:"responseLimit"`
 }
